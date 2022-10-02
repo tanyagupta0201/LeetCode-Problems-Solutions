10da2062039f5a08dfa8da2d70da9d4330422dc3
Tasmiya <tasmiyakhan2798@gmail.com>
2022-10-02 23:44:49 +0530

    Code formatted

--- C++/83_RemoveDuplicatesFromSortedList.cpp
+++ C++/83_RemoveDuplicatesFromSortedList.cpp
@@ -4,20 +4,19 @@ Date: 2 October 2022
 */
 
 class Solution {
-public:
-    ListNode* deleteDuplicates(ListNode* head) {
-        ListNode* ptr=head;
-        if(head==nullptr){
-            return nullptr;
+  public:
+    ListNode * deleteDuplicates(ListNode * head) {
+      ListNode * ptr = head;
+      if (head == nullptr) {
+        return nullptr;
+      }
+      while (ptr -> next != nullptr) {
+        if (ptr -> val == ptr -> next -> val) {
+          ptr -> next = ptr -> next -> next;
+        } else {
+          ptr = ptr -> next;
         }
-        while(ptr->next!=nullptr){
-            if(ptr->val==ptr->next->val){
-                ptr->next=ptr->next->next;
-            }
-            else{
-            ptr=ptr->next;
-            }
-        }
-        return head;
+      }
+      return head;
     }
 };
