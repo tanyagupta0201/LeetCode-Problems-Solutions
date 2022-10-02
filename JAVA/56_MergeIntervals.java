01010971476b144348c811561cce3c92876a0c92
Aneesh Gupta <77202061+aneeshgupta25@users.noreply.github.com>
2022-10-03 00:21:56 +0530

    Formatted Code

--- JAVA/56_MergeIntervals.java
+++ JAVA/56_MergeIntervals.java
@@ -15,27 +15,27 @@ IDEOLOGY -> Sort the given elements of the array. Then merge 2 meetings if start
 
 
 class Solution {
-    public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a,b)-> Integer.compare(a[0], b[0]));    //This sorting will sort the meetings according to their start time.
-        
-        Stack<int[]> st = new Stack<>();
-        st.push(intervals[0]);    //Initially, we'll push the first meeting element into the stack.
-     
-        for(int i = 1; i < intervals.length; i++){
-            if(st.peek()[1] >= intervals[i][0]){
-                int[] arr = st.pop();
-                arr[1] = Math.max(arr[1], intervals[i][1]);
-                st.push(arr);
-            } else {
-                st.push(intervals[i]);
-            }
-        }
-        int[][] ans = new int[st.size()][2];
-        int index = ans.length-1;
-        while(index >= 0){
-            ans[index] = st.pop();
-            index--;
-        }
-        return ans;
+  public int[][] merge(int[][] intervals) {
+    Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0])); //This sorting will sort the meetings according to their start time.
+
+    Stack < int[] > st = new Stack < > ();
+    st.push(intervals[0]); //Initially, we'll push the first meeting element into the stack.
+
+    for (int i = 1; i < intervals.length; i++) {
+      if (st.peek()[1] >= intervals[i][0]) {
+        int[] arr = st.pop();
+        arr[1] = Math.max(arr[1], intervals[i][1]);
+        st.push(arr);
+      } else {
+        st.push(intervals[i]);
+      }
     }
+    int[][] ans = new int[st.size()][2];
+    int index = ans.length - 1;
+    while (index >= 0) {
+      ans[index] = st.pop();
+      index--;
+    }
+    return ans;
+  }
 }
