c1fa3e22c41e6173be7dbd08de225ff7b4a2de6d
Nivesh <59831140+Niveshpai@users.noreply.github.com>
2022-10-03 23:10:01 +0530

    Create 16_3Sum_Closest.cpp

---
+++ C++/16_3Sum_Closest.cpp
@@ -0,0 +1,31 @@
+// Time:  O(n^2)
+// Space: O(1)
+// Program for 3sum closest
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        sort(begin(nums), end(nums));
+        int result = 0, min_diff = numeric_limits<int>::max();
+        for (int i = size(nums) - 1; i >= 2; --i) {
+            if (i + 1 < size(nums) && nums[i] == nums[i + 1]) {
+                continue;
+            }
+            int left = 0, right = i - 1;
+            while (left < right) {
+                const auto& total = nums[left] + nums[right] + nums[i];
+                if (total < target) {
+                    ++left;
+                } else if (total > target) {
+                    --right;
+                } else {
+                    return target;
+                }
+                if (abs(total - target) < min_diff) {
+                    min_diff = abs(total - target);
+                    result = total;
+                }
+            }
+        }
+        return result;
+    }
+};
