02a47e14d4bd9224c61a4139a4349d8076803e2e
Adarsh Gupta <112967201+adarshgupta404@users.noreply.github.com>
2022-10-03 17:14:57 +0530

    Code for issue #45

--- C++/12_IntegerToRoman.cpp
+++ C++/12_IntegerToRoman.cpp
@@ -1,20 +1,22 @@
 // Name: Adarsh Gupta
 // Date: 03/10/2022
 
-class Solution {
-public:
-    string intToRoman(int n) {
- string romans[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-int nums[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-string ans = "";
-for (int i = 0; i < 13; ++i)
+class Solution 
 {
-    while(n-nums[i]>=0)
+    public:
+    string intToRoman(int n) 
     {
-        ans = ans + romans[i];
-        n = n - nums[i];
-    }
-}
-return ans;
+        string romans[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int nums[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        string ans = "";
+        for (int i = 0; i < 13; ++i)
+        {
+            while(n-nums[i]>=0)
+            {
+                ans = ans + romans[i];
+                n = n - nums[i];
+            }
+        }   
+        return ans;
     }
 };
