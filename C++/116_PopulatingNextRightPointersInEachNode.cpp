e8bd35d3479066de52b1e6983940adc9c1397554
karthiksenthil28 <student.mail.karthik@gmail.com>
2022-10-03 15:13:18 +0530

    This is the Solution for Leetcode problem number 116

---
+++ C++/116_PopulatingNextRightPointersInEachNode.cpp
@@ -0,0 +1,45 @@
+/*
+Name : Karthik S
+Date : 03 October 2022
+*/
+
+// Link to Question
+// https://leetcode.com/problems/populating-next-right-pointers-in-each-node/submissions/ 
+
+class Solution {
+  public:
+    Node * connect(Node * root) {
+      queue < Node * > q;
+      Node * root1 = root;
+      if (root1 == NULL)
+        return NULL;
+
+      q.push(root1);
+
+      while (!q.empty()) {
+        int n = q.size();
+        Node * prev = NULL;
+        for (int i = 0; i < n; i++) {
+          Node * curr = q.front();
+          q.pop();
+
+          if (curr -> left != NULL)
+            q.push(curr -> left);
+          if (curr -> right != NULL)
+            q.push(curr -> right);
+
+          if (prev == NULL) {
+            prev = curr;
+            prev -> next = NULL;
+            continue;
+          }
+
+          prev -> next = curr;
+          prev = curr;
+          if (i == n - 1)
+            curr -> next = NULL;
+        }
+      }
+      return root;
+    }
+};
