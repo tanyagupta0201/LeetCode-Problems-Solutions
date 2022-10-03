ee6ddd33af7d86e4fde3528e5940ed888c89309a
Himanshu Singh <chamyalhemsingh@gmail.com>
2022-10-03 23:57:26 +0530

    Formatted my code using the link

--- C++/622_DesignCircularQueue.cpp
+++ C++/622_DesignCircularQueue.cpp
@@ -2,43 +2,42 @@
 //  Date: 3/10/2022
 
 class MyCircularQueue {
-private:
-    vector<int>que;
-    int front=0, cnt=0;
-public:
-    MyCircularQueue(int k): que(k) {
-    }
-    
-    bool enQueue(int value) {
-      if(isFull())return false;
-        que[(front + cnt++)%que.size()] = value;
-        return true;
-    }
-    
-    bool deQueue() {
-        if(isEmpty())return false;
-        front = (front+1)%que.size();
-        --cnt;
-        return true;
-    }
-    
-    int Front() {
-        if(isEmpty())return -1;
-        return que[front];
-    }
-    
-    int Rear() {
-        if(isEmpty())return -1;
-        return que[(front+cnt-1)%que.size()];
-    }
-    
-    bool isEmpty() {
-        return !cnt;
-    }
-    
-    bool isFull() {
-        return que.size()==cnt;
-    }
+  private:
+    vector < int > que;
+  int front = 0, cnt = 0;
+  public:
+    MyCircularQueue(int k): que(k) {}
+
+  bool enQueue(int value) {
+    if (isFull()) return false;
+    que[(front + cnt++) % que.size()] = value;
+    return true;
+  }
+
+  bool deQueue() {
+    if (isEmpty()) return false;
+    front = (front + 1) % que.size();
+    --cnt;
+    return true;
+  }
+
+  int Front() {
+    if (isEmpty()) return -1;
+    return que[front];
+  }
+
+  int Rear() {
+    if (isEmpty()) return -1;
+    return que[(front + cnt - 1) % que.size()];
+  }
+
+  bool isEmpty() {
+    return !cnt;
+  }
+
+  bool isFull() {
+    return que.size() == cnt;
+  }
 };
 
 /**
