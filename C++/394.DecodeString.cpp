df7b5426b3cd06f38eca54c903b7cdc37177f7f4
Tathagat <tathagat1@Tathagats-MacBook-Air.local>
2022-10-04 01:01:09 +0530

    solution for problem no 394 decode string

---
+++ C++/394.DecodeString.cpp
@@ -0,0 +1,46 @@
+//Tathagat Vats
+//4/10/2022 
+class Solution {
+public:
+
+    string decodeString(string s) {
+        stack<char> st;                          //create a stack of characters
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] != ']') {             
+                st.push(s[i]); 
+            }
+            else{
+                string curr_str = "";
+                
+                while(st.top() != '['){                   
+                    curr_str = st.top() + curr_str ;
+                    st.pop();
+                }
+                
+                st.pop();  
+                string number = "";
+                
+                // for calculating number
+                
+                while(!st.empty() && isdigit(st.top())){
+                    number = st.top() + number;
+                    st.pop();
+                }
+                int k_time = stoi(number);    // convert string to number
+                
+                while(k_time--){
+                    for(int p = 0; p < curr_str.size() ; p++)
+                        st.push(curr_str[p]);
+                }
+            }
+        }
+        
+        s = "";
+        while(!st.empty()){
+            s = st.top() + s;
+            st.pop();
+        }
+        return s;
+        
+    }
+};
