422ce7be94573029069b69832392b786336e8e7a
harshshukla18 <harsh.19b101028@abes.ac.in>
2022-10-04 17:36:32 +0530

    Create 75_SortColors.py

---
+++ PYTHON/75_SortColors.py
@@ -0,0 +1,34 @@
+#Name: Harsh Shukla
+#Problem: 75 Sort Colors Leetcode
+
+
+def func( arr, arr_size):   
+  low = 0  
+  high = arr_size - 1  
+  mid = 0  
+  while mid <= high:   
+    if arr[mid] == 0:   
+      arr[low],arr[mid] = arr[mid],arr[low]   
+      low = low + 1  
+      mid = mid + 1  
+    elif arr[mid] == 1:   
+      mid = mid + 1  
+    else:   
+      arr[mid],arr[high] = arr[high],arr[mid]   
+      high = high - 1  
+  return arr   
+      
+# Function to print array   
+def printArray(arr):   
+    for k in arr:   
+        print (k),
+    print()
+      
+# Driver Program   
+arr = [0, 1, 0, 1, 1, 2, 0, 1, 2]   
+arr_size = len(arr)  
+print(" Array before executing the algorithm: ",   
+printArray(arr))
+arr = func(arr, arr_size)   
+print ("Array after executing the DNFS algorithm: ",   
+printArray(arr))
