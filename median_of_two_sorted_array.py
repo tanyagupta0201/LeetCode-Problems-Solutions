0cf17f240babdd9a16a88cd05b3459629ebd82b6
Shivang Saxena <61111601+techyshiv@users.noreply.github.com>
2022-10-05 20:50:02 +0530

    Leet Code Problem Solution Median of Two Sorted Array
    It is a Leet Code Hard Problem in which we have to give a two sorted array and we have to return the median of that sorted array implementation done in python3

---
+++ median_of_two_sorted_array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findMedianSortedArrays(self,nums1,nums2):
+        sorted_array = nums1 + nums2
+        sorted_array = sorted(sorted_array)
+        print(sorted_array)
+        length = len(sorted_array)
+        if(length % 2 == 0):
+            middle_element = int(length/2)-1
+            return (sorted_array[middle_element] + sorted_array[middle_element+1]) /(2)
+        else:
+            middle_element = int(length/2)
+            return sorted_array[middle_element]
