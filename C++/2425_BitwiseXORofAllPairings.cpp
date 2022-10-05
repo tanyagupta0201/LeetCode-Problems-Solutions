b24c80c7909960938d569edb999b2c8b1e35ac3f
Nitesh <niteshreddy257@gamil.com>
2022-10-05 20:21:50 +0530

    fomatted

--- C++/2425_BitwiseXORofAllPairings.cpp
+++ C++/2425_BitwiseXORofAllPairings.cpp
@@ -1,6 +1,6 @@
 /*
-G Nitesh Kuamr Reddy
-05/10/2022
+//  G Nitesh Kuamr Reddy
+//  05/10/2022
 */
 /*
 You are given two 0-indexed arrays, nums1 and nums2, consisting of non-negative integers. There exists another array, nums3, which contains the bitwise XOR of all pairings of integers between nums1 and nums2 (every integer in nums1 is paired with every integer in nums2 exactly once).
