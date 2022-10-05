0a6db9221036524469b5d88568e0026da8fafad8
Naman027 <ng24645@gmail.com>
2022-10-05 12:41:00 +0530

    Code formatted according to Link

--- C++/1578_MinimumTimeToMakeRopeColorful.cpp
+++ C++/1578_MinimumTimeToMakeRopeColorful.cpp
@@ -2,22 +2,22 @@
 // Date: 4/10/2022
 
 class Solution {
-public:
-    int minCost(string colors, vector<int>& neededTime) {
-        int n = colors.size();
-        int ans = 0;
-        for(int i=0;i<n;i++){
-            int j = i;
-            int sum = 0, ma = 0;
-            while(j<n && colors[j]==colors[i]){
-                sum+=neededTime[j];
-                ma = max(ma,neededTime[j]);
-                j++;
-            }
-            j--;
-            if(j-i+1>=2) ans+=sum-ma;
-            i = j;
+  public:
+    int minCost(string colors, vector < int > & neededTime) {
+      int n = colors.size();
+      int ans = 0;
+      for (int i = 0; i < n; i++) {
+        int j = i;
+        int sum = 0, ma = 0;
+        while (j < n && colors[j] == colors[i]) {
+          sum += neededTime[j];
+          ma = max(ma, neededTime[j]);
+          j++;
         }
-        return ans;
+        j--;
+        if (j - i + 1 >= 2) ans += sum - ma;
+        i = j;
+      }
+      return ans;
     }
 };
