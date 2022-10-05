1109b9c428c0cbf449ff7049cf7f3040c40972a5
souvikpal2000 <spal62588@gmail.com>
2022-10-05 18:53:29 +0530

    Added Comments

--- JAVA/3_LongSubStrWithoutReptChars.java
+++ JAVA/3_LongSubStrWithoutReptChars.java
@@ -26,26 +26,30 @@ Notice that the answer must be a substring, "pwke" is a subsequence and not a su
 
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int length = s.length();
-        String visitedCharacters = "";
+        int length = s.length(); // Length of the String
+        String visitedCharacters = ""; // Visited Characters will be appended in this Variable (SubString)
 
         int i = 0;
-        int max = 0;
+        int max = 0; // Length of the Longest Substring
 
         while (i < length) {
             char character01 = s.charAt(i);
-            visitedCharacters = visitedCharacters + character01;
+            visitedCharacters = visitedCharacters + character01; // Character Appended
             int j = (i + 1);
             while (j < length) {
                 char character02 = s.charAt(j);
+
+                // Checking whether the Character is present in the Visited Characters or not
                 if (visitedCharacters.indexOf(character02) == -1) {
-                    visitedCharacters = visitedCharacters + character02;
+                    visitedCharacters = visitedCharacters + character02; // Character Appended
+
+                    //Checking Whether Inner Loop parsed throughout the Leftover String without any Repeatation 
                     if (j == (length - 1)) {
-                        max = Math.max(max, visitedCharacters.length());
+                        max = Math.max(max, visitedCharacters.length()); // Checking Maximum between Last Substring & recently created Substring
                         visitedCharacters = "";
                     }
                 } else {
-                    max = Math.max(max, visitedCharacters.length());
+                    max = Math.max(max, visitedCharacters.length()); // Checking Maximum between Last Substring & recently created Substring
                     visitedCharacters = "";
                     break;
                 }
@@ -54,6 +58,7 @@ class Solution {
             i++;
         }
 
+        //Condition for the Single Character String
         if (i == 1) {
             max = 1;
         }
