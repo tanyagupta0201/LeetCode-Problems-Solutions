81874c643358170ba916be8786ae3d4e699be60f
Garvit414 <103891145+Garvit414@users.noreply.github.com>
2022-10-05 14:27:17 +0530

    Update 191_NumberOf1Bits.cpp

--- C++/191_NumberOf1Bits.cpp
+++ C++/191_NumberOf1Bits.cpp
@@ -1,12 +1,12 @@
 // Name: Garvit Agarwal
-//Date: 05/10/2022
+// Date: 05/10/2022
 
 class Solution {
   public:
     int hammingWeight(uint32_t n) {
       int count = 0;
       while (n > 0) {
-        //Checking  last digit is 1 or not
+        // Checking  last digit is 1 or not
         if (n & 1) {
           count++;
         }
