7646c483e7876c902c3ff5c9fe7da5c8e914ca43
Naman Goyal <60535864+Naman027@users.noreply.github.com>
2022-10-05 21:28:14 +0530

    Added Comments to Prob No 1578

--- C++/1578_MinimumTimeToMakeRopeColorful.cpp
+++ C++/1578_MinimumTimeToMakeRopeColorful.cpp
@@ -6,6 +6,7 @@ class Solution {
     int minCost(string colors, vector < int > & neededTime) {
       int n = colors.size();
       int ans = 0;
+      // Iterating over the colors array and checking the equal colors values 
       for (int i = 0; i < n; i++) {
         int j = i;
         int sum = 0, ma = 0;
@@ -15,6 +16,7 @@ class Solution {
           j++;
         }
         j--;
+        // Finding the maximum value and subtracting it from the sum if no of same color values >= 2, taking only one value
         if (j - i + 1 >= 2) ans += sum - ma;
         i = j;
       }
