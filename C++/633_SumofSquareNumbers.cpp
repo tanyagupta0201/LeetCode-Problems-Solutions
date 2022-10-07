3ec1ec5b45416974ae92ffa8b603918657d76d8c
roy2pushkar <roy2000pushkar.com>
2022-10-07 16:44:30 +0530

    633_SumofSquareNumbers.cpp

---
+++ C++/633_SumofSquareNumbers.cpp
@@ -0,0 +1,28 @@
+//Name :- Pushkar Roy
+//Date :- 05-10-2022
+
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+      long long int l = 0;
+
+        long long int r = sqrt(c);
+        while(l<=r)
+        {
+            long long int  value = (l*l) + (r*r);
+            if(value == c)
+            {
+                return true;
+            }
+            else if(value > c)
+            {
+                r--;
+            }
+            else if(value < c)
+            {
+                l++;
+            }
+        }
+        return false;
+    }
+};
