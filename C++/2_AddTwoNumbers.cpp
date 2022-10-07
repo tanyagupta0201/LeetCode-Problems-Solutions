78a554bd80290c5def3e8c4f1fbd8b25c67d8fff
Amit_Bisht <singhbishtamit2@gmail.com>
2022-10-07 11:38:18 +0530

    leet

--- C++/2_AddTwoNumbers.cpp
+++ C++/2_AddTwoNumbers.cpp
@@ -1,35 +1,36 @@
-// Author - Amit singh Bisht
-//   Date-05/10/2022
+//  Name:  Amit singh Bisht
+//  Date:  07/10/2022
+
 class Solution
 {
 public:
     ListNode *addTwoNumbers(ListNode *l1, ListNode *l2)
     {
         int r = 0;
-        ListNode *head, *temp = new ListNode;
+        ListNode *head, *temp = new ListNode; // create a new listnode and allocate memory to it
         head = NULL;
         int c = 0;
         while (l1 != 0 && l2 != 0)
         {
             ListNode *a = new ListNode;
-            int sum = l1->val + l2->val + r;
+            int sum = l1->val + l2->val + r; // add the data in both the list
             if (sum < 10)
             {
-                a->val = sum;
+                a->val = sum;  // if data is less than 10 push it to new list
                 r = 0;
             }
             else
             {
-                a->val = sum % 10;
+                a->val = sum % 10; // data is greater than 10 so push the remainder two to the list and store its carry
                 r = sum / 10;
             }
             if (head == NULL)
             {
-                head = a;
+                head = a;     // if head is null than point it to newly created list a
             }
             else
             {
-                head->next = a;
+                head->next = a; // keep tracking previous and next nodes in list
                 head = a;
             }
             if (c == 0)
@@ -40,7 +41,7 @@ public:
             l1 = l1->next;
             l2 = l2->next;
         }
-        while (l1 != 0)
+        while (l1 != 0)   //if  the list 1 is greater than list 2
         {
             ListNode *a = new ListNode;
             int sum = l1->val + r;
@@ -71,7 +72,7 @@ public:
             }
             l1 = l1->next;
         }
-        while (l2 != 0)
+        while (l2 != 0) //if  the list 2 is greater than list 1
         {
             ListNode *a = new ListNode;
             int sum = l2->val + r;
@@ -108,6 +109,6 @@ public:
             head->next = a;
             head = a;
         }
-        return temp;
+        return temp; // return the address of head node of the final list
     }
 };
