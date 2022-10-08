1a28f966826cd45d18d1209a1a3c58429675b0a8
MaYiLagann <lhwshy@naver.com>
2022-10-09 00:06:01 +0900

    Add description

--- JAVA/273_IntegerToEnglishWords.java
+++ JAVA/273_IntegerToEnglishWords.java
@@ -1,5 +1,6 @@
 // Name: MaYiLagann
 // Date: 08/10/2022
+// Description: Problem link: (https://leetcode.com/problems/integer-to-english-words/)
 
 class Solution {
     public String numberToWords(int num) {
