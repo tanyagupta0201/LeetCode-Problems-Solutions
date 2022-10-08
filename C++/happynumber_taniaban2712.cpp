deb2d95b41620e56619994f6dab291e2cc1f5d55
Tania Banerjee <110692684+taniaban2712@users.noreply.github.com>
2022-10-08 13:05:30 +0530

    new commit
    Uploading new files on the contribution

---
+++ C++/happynumber_taniaban2712.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        int x=n;
+        int sum=0;
+        while(x){
+           // sum=0;
+            int h=x%10;
+            sum+=h*h;
+            x=x/10;
+        }
+        if(sum==1||sum==7)
+            return true;
+        else if(sum<10)
+            return false;
+        else
+            return isHappy(sum);
+        
+    }
+};
+//using c++ to write the program
