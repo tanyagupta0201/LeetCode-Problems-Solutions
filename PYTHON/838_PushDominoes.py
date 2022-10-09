fdf47f508fa377de98cd9615a71bfc37a55e6e4e
Himanshigarg08 <92046281+Himanshigarg08@users.noreply.github.com>
2022-10-09 17:24:53 +0530

    Update 838_PushDominoes.py

--- PYTHON/838_PushDominoes.py
+++ PYTHON/838_PushDominoes.py
@@ -6,7 +6,7 @@ class Solution:
         prevString = ""
         current = dominoes
 
-        if len(dominoes) == 1:
+        if len(dominoes) == 1:  # If there is only one domino
             return dominoes
 
         while prevString != current:
@@ -16,21 +16,21 @@ class Solution:
 
                 if i != 0 and i != len(prevString) - 1:
                     if prevString[i] == ".":
-                        if prevString[i-1] == "R" and prevString[i+1] != "L":
-                            current += "R"
-                        elif prevString[i+1] == "L" and prevString[i-1] != "R":
-                            current += "L"
+                        if prevString[i-1] == "R" and prevString[i+1] != "L":  # "R.." or "R.R" -- in both cases it will expend force on ith domino towards right
+                            current += "R"                  
+                        elif prevString[i+1] == "L" and prevString[i-1] != "R": # "..L" or "L.L" -- in both cases it will expend force on ith domino towards left
+                            current += "L"                  
                         else:
-                            current += "."
+                            current += "."                  # neither right nor left
                     else:
                         current += prevString[i]
                 elif i == 0:
-                    if prevString[0] == "." and prevString[i+1] == "L":
+                    if prevString[0] == "." and prevString[i+1] == "L": # ".L" -- resultant force will be towards left
                         current += "L"
-                    else:
+                    else:  
                         current += prevString[0]
 
-                elif i == len(prevString)-1:
+                elif i == len(prevString)-1:   # "R." -- resultant force will be towards right
                     if prevString[i] == "." and prevString[i-1] == "R":
                         current += "R"
                     else:
