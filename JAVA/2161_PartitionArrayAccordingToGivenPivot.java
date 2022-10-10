40f18db93a2cd6785919984e23a7235f1eaac8c7
Aneesh Gupta <77202061+aneeshgupta25@users.noreply.github.com>
2022-10-10 23:03:28 +0530

    Update 2161_PartitionArrayAccordingToGivenPivot.java
    Formatted Code

--- JAVA/2161_PartitionArrayAccordingToGivenPivot.java
+++ JAVA/2161_PartitionArrayAccordingToGivenPivot.java
@@ -1,32 +1,32 @@
-// Aneesh Gupta
-// 10-10-2022
+// Name: Aneesh Gupta
+// Date: 10/10/2022
 
 class Solution {
-    public int[] pivotArray(int[] nums, int pivot) {
-    
-        int pivotFreq = 0;
-        ArrayDeque<Integer> smele = new ArrayDeque<>();
-        ArrayDeque<Integer> lgele = new ArrayDeque<>();
-        
-        for(int val : nums){
-            if(val < pivot){
-                smele.add(val);
-            } else if(val == pivot) {
-                pivotFreq++;
-            } else {
-                lgele.add(val);
-            }
-        }
-        
-        for(int i = 0; i < nums.length; i++){
-            if(smele.size() > 0){
-                nums[i] = smele.remove();
-            } else if(pivotFreq-- > 0) {
-                nums[i] = pivot;
-            } else {
-                nums[i] = lgele.remove();
-            }
-        }
-        return nums;
+  public int[] pivotArray(int[] nums, int pivot) {
+
+    int pivotFreq = 0;
+    ArrayDeque < Integer > smele = new ArrayDeque < > ();
+    ArrayDeque < Integer > lgele = new ArrayDeque < > ();
+
+    for (int val: nums) {
+      if (val < pivot) {
+        smele.add(val);
+      } else if (val == pivot) {
+        pivotFreq++;
+      } else {
+        lgele.add(val);
+      }
     }
+
+    for (int i = 0; i < nums.length; i++) {
+      if (smele.size() > 0) {
+        nums[i] = smele.remove();
+      } else if (pivotFreq--> 0) {
+        nums[i] = pivot;
+      } else {
+        nums[i] = lgele.remove();
+      }
+    }
+    return nums;
+  }
 }
