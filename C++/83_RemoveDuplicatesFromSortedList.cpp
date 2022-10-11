57d9327cf54935a3721e0ef8b1aa90721e59646d
Jaymin Lathiya <93418767+Jaymin-Lathiya@users.noreply.github.com>
2022-10-11 18:43:05 +0530

    Update 83_RemoveDuplicatesFromSortedList.cpp

--- C++/83_RemoveDuplicatesFromSortedList.cpp
+++ C++/83_RemoveDuplicatesFromSortedList.cpp
@@ -20,3 +20,38 @@ class Solution {
       return head;
     }
 };
+
+
+// Another Implementation :
+
+
+
+// Node *uniqueSortedList(Node *head)
+// {
+
+//     //  case 1: if list is empty
+//     if (head == NULL)
+//         return NULL;
+
+//     // case 2 : if the given list is non - empty
+
+//     Node *curr = head;
+
+//     while (curr != NULL)
+//     {
+
+//         if ((curr->next != NULL) && curr->data == curr->next->data)
+//         {
+//             Node *next_next = curr->next->next;
+//             Node *nodeToDelete = curr->next;
+//             delete (nodeToDelete);
+//             curr->next = next_next;
+//         }
+//         else
+//         {
+//             curr = curr->next;
+//         }
+//     }
+
+//     return head;
+// }
