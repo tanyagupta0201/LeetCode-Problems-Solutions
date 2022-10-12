58f71bb9668bec2c0b370728f59fd088f09e4081
Tanisha Garg <65440985+tanishagarg1803@users.noreply.github.com>
2022-10-12 14:36:18 +0530

    Update 91_DecodeWays.cpp

--- C++/91_DecodeWays.cpp
+++ C++/91_DecodeWays.cpp
@@ -1,28 +1,32 @@
 // Nmae: Tanisha Garg
-// Date: 11/10/2002
+// Date: 12/10/2022
 
 class Solution {
-public:
+  public:
     int dp[101];
-    int decoding(string s,int i) {
-        if(i==s.length()){
-            return 1;
-        }
-        if(dp[i]!=-1){
-            return dp[i];
-        }
-        long long int c=0;
-        if(s[i]!='0'){
-            c+=decoding(s,i+1);
-        }
-        if(i<s.length()-1 && s[i]!='0' && ((s[i]-'0')*10+s[i+1]-'0')<=26){
-            c+=decoding(s,i+2);
-        }
-        return dp[i]=c;
-            
+  int decoding(string s, int i) {
+    if (i == s.length()) {
+      return 1;
     }
-    int numDecodings(string s){
-        memset(dp,-1,sizeof(dp));
-        return decoding(s,0);
+    // if we already have an entry on dp table
+    if (dp[i] != -1) {
+      return dp[i];
     }
+    long long int c = 0;
+    // for single digit
+    if (s[i] != '0') {
+      c += decoding(s, i + 1);
+    }
+    // In all other cases construct a two-digit number and apply
+    if (i < s.length() - 1 && s[i] != '0' && ((s[i] - '0') * 10 + s[i + 1] - '0') <= 26) {
+      c += decoding(s, i + 2);
+    }
+    return dp[i] = c;
+
+  }
+  int numDecodings(string s) {
+    // declaring 1D dp table
+    memset(dp, -1, sizeof(dp));
+    return decoding(s, 0);
+  }
 };
