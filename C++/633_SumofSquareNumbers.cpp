c05dc2e95d2687207ec75adda42dd152ccb7ff2b
roy2pushkar <roy2000pushkar.com>
2022-10-13 20:30:08 +0530

    633_SumofSquareNumbers.cpp

--- C++/633_SumofSquareNumbers.cpp
+++ C++/633_SumofSquareNumbers.cpp
@@ -3,26 +3,32 @@
 
 class Solution {
 public:
+    //make function for check square or not
     bool judgeSquareSum(int c) {
       long long int l = 0;
 
         long long int r = sqrt(c);
+        //check condition for loop
         while(l<=r)
-        {
+        {    //find range for checking
             long long int  value = (l*l) + (r*r);
+            //if value matchecd with given number return true
             if(value == c)
             {
                 return true;
             }
+            //if value greater than given number
             else if(value > c)
             {
                 r--;
             }
+            //if value less than given number
             else if(value < c)
             {
                 l++;
             }
         }
+        //if value not matched
         return false;
     }
 };
