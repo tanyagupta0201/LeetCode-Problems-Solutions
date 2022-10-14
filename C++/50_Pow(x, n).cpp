70287be9b914d61fd41b52821dfee59887aa1db5
Chandan Kumar Sahu <chandansahuji123@gmail.com>
2022-10-14 21:05:03 +0530

    Updated the code

--- C++/50_Pow(x, n).cpp
+++ C++/50_Pow(x, n).cpp
@@ -1,19 +1,46 @@
+/*NAME: Chandan Sahu
+DATE: 14/10/2022
+
+The problem can be recursively defined by:
+
+power(x, n) = power(x, n / 2) * power(x, n / 2);        // if n is even
+power(x, n) = x * power(x, n / 2) * power(x, n / 2);    // if n is odd
+*/
+
 class Solution {
-public:
+
+  public:
+
     double myPow(double x, int n) {
-        
-        if(n==0){
-            return 1;
-        }
-        if (n < 0) { 
-            n = abs(n);
-            x = 1/x;
-        }
-        if(n%2==0){
-            return myPow(x*x,n/2);
-        
-        }else{
-            return x*myPow(x*x,n/2);
-        }
+
+      // If x^0 return 1
+
+      if (n == 0) {
+
+        return 1;
+
+      }
+      // if n is negative
+
+      if (n < 0) {
+
+        n = abs(n);
+
+        x = 1 / x;
+
+      }
+      //if n is even
+      if (n % 2 == 0) {
+
+        return myPow(x * x, n / 2);
+
+      } // if n is odd 
+      else {
+
+        return x * myPow(x * x, n / 2);
+
+      }
+
     }
+
 };
