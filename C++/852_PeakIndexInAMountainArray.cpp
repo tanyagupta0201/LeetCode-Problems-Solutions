eec656c8916ea44bef2c30dae7d7b5ca8ed1be39
Pranav Avasthi <96632158+PranavAvasthi@users.noreply.github.com>
2022-10-15 08:16:48 +0530

    Update 852_PeakIndexInAMountainArray.cpp

--- C++/852_PeakIndexInAMountainArray.cpp
+++ C++/852_PeakIndexInAMountainArray.cpp
@@ -4,12 +4,15 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
+        // first or last element is peak element
         int s = 0;
         int e = arr.size() - 1;
         
         int mid = s + (e-s)/2;
         
+        // check for every other element
         while(s<e) {
+             // check if the neighbors are smaller
             if(arr[mid] < arr[mid+1]) {
                 s = mid + 1;
             }
