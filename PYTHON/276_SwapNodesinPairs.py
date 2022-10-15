5ae6a8834d698b25b18690fd1f92a71f625df640
chharikha2003 <csharikha03@gmail.com>
2022-10-15 13:54:32 +0530

    added swap nodes in pairs in python

--- PYTHON/276_SwapNodesinPairs.py
+++ PYTHON/276_SwapNodesinPairs.py
@@ -1,11 +1,15 @@
-#%%
 #Author name:Chengalva Sai Harikha
 #Date:15/10/2022
+#Time complexity:O(n)
+#Space Complexity:O(1)
 class Solution(object):
     def swapPairs(self,head):
+       #if head is None then the linked list is empty so return head
        if not head:
            return head
+       #3 variables assigned to store 1st node and next node
        previous,current,answer=None,head,head.next
+       #loop runs till current or current.next is not None 
        while current and current.next:
            adj=current.next
            if previous:
