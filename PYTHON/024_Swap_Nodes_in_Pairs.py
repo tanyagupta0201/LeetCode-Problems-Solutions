4c4334f8cbf41ea49f96b4a0e8a1c5c311a14bc2
chharikha2003 <csharikha03@gmail.com>
2022-10-15 13:21:47 +0530

    added python code to swap nodes in pairs

---
+++ PYTHON/024_Swap_Nodes_in_Pairs.py
@@ -0,0 +1,13 @@
+#%%
+class Solution(object):
+    def swapPairs(self,head):
+       if not head:
+           return head
+       previous,current,answer=None,head,head.next
+       while current and current.next:
+           adj=current.next
+           if previous:
+               previous.next=adj
+           current.next,adj.next=adj.next,current
+           previous,current=current,current.next
+       return answer or head
