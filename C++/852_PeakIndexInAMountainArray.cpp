d0c9016b0b3b84917951f6ea8fe8163616e92000
Tanya Gupta <66638392+tanyagupta0201@users.noreply.github.com>
2022-10-16 16:47:17 +0530

    Update 852_PeakIndexInAMountainArray.cpp

--- C++/852_PeakIndexInAMountainArray.cpp
+++ C++/852_PeakIndexInAMountainArray.cpp
@@ -8,18 +8,18 @@ public:
         int s = 0;
         int e = arr.size() - 1;
         
-        int mid = s + (e-s)/2;
+        int mid = s + (e - s) / 2;
         
         // check for every other element
-        while(s<e) {
+        while(s < e) {
              // check if the neighbors are smaller
-            if(arr[mid] < arr[mid+1]) {
+            if(arr[mid] < arr[mid + 1]) {
                 s = mid + 1;
             }
             else {
                 e = mid;
             }
-            mid = s + (e-s)/2;
+            mid = s + (e - s) / 2;
         }
         return s;
     }
