68f04bec3865a460608643e5d78c1e08a1e86477
chharikha2003 <csharikha03@gmail.com>
2022-10-16 17:40:11 +0530

    swap nodes in pairs in python

--- PYTHON/276_SwapNodesinPairs.py
+++ PYTHON/276_SwapNodesinPairs.py
@@ -2,18 +2,27 @@
 #Date:15/10/2022
 #Time complexity:O(n)
 #Space Complexity:O(1)
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution(object):
-    def swapPairs(self,head):
-       #if head is None then the linked list is empty so return head
-       if not head:
-           return head
-       #3 variables assigned to store 1st node and next node
-       previous,current,answer=None,head,head.next
-       #loop runs till current or current.next is not None 
-       while current and current.next:
-           adj=current.next
-           if previous:
-               previous.next=adj
-           current.next,adj.next=adj.next,current
-           previous,current=current,current.next
-       return answer or head
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head:
+            return head
+        #3 variables assigned to store 1st node and next node
+        previous,current,answer=None,head,head.next
+        #loop runs till current or current.next is not None 
+        while current and current.next:
+            #adj variable stores the next variable to current
+            adj=current.next
+            if previous:
+                previous.next=adj
+            current.next,adj.next=adj.next,current
+            previous,current=current,current.next
+        return answer or head
