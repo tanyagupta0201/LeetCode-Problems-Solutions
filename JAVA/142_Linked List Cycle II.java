ffd458a31b084b961b56aa68d494d5c3d9a84055
Tanya Gupta <66638392+tanyagupta0201@users.noreply.github.com>
2022-10-17 22:46:29 +0530

    Update 142_Linked List Cycle II.java

--- JAVA/142_Linked List Cycle II.java
+++ JAVA/142_Linked List Cycle II.java
@@ -1,6 +1,8 @@
-//Name: Supriya
-//Date:10/10/2022
-/*Question:Given the head of a linked list, return the position of node where the cycle begins. If there is no cycle, return null.
+// Name: Supriya
+// Date:10/10/2022
+
+/*
+Question: Given the head of a linked list, return the position of node where the cycle begins. If there is no cycle, return null.
 Input: head = [3,2,0,-4], pos = 1
 Output: tail connects to node index 1
 Explanation: There is a cycle in the linked list, where tail connects to the second node.
@@ -8,8 +10,11 @@ ________________________________________________________________________________
 Approach: Fast and Slow pointer Method
 step-1: Find the length of the cycle
 step-2: Make first and second pointer which points to head and traverse second pointer upto length of the cycle
-step-3: Traverse while first and second pointers are not equals null */
-//SOLUTION
+step-3: Traverse while first and second pointers are not equals null 
+*/
+
+// SOLUTION
+
 public class Solution {
   public ListNode detectCycle(ListNode head) {
     ListNode fast = head;
