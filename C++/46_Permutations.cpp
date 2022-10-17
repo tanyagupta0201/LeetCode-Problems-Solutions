8c08b29d4619706b68b5163e3bee8b1e2ca2d301
vinay_pseudo <kumarvinay.230801@gmail.com>
2022-10-17 17:33:51 +0530

    Added comments to earlier commit

--- C++/46_Permutations.cpp
+++ C++/46_Permutations.cpp
@@ -1,18 +1,25 @@
-//Name : A VINAY KUMAR
-//Date : 06/10/2022
+// Name : A VINAY KUMAR
+// Date : 06/10/2022
 
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
+        // base case
         if(nums.size() <= 1) {
             return {nums};
         }
+
+        //vector to store the permutations
         vector<vector<int>> ans;
         for(auto it = nums.begin();it != nums.end();it++) {
             vector<int> temp(nums.begin(), nums.end());
+
+            //removing the current item and receiving all the permutations of rest
             temp.erase(find(temp.begin(),temp.end(),*it));
             vector<vector<int>> tempans  = permute(temp);
             for(auto x: tempans) {
+
+                // adding the removed item to all the permutations received
                 x.insert(x.begin(), *it);
                 ans.push_back(x);
             }
