e4446776ecf789d4b7735f23ca0012a6a739a714
Tanya Gupta <66638392+tanyagupta0201@users.noreply.github.com>
2022-10-17 19:00:24 +0530

    Update 2259_RemoveDigitFromNumberToMaximizeResult.cpp

--- C++/2259_RemoveDigitFromNumberToMaximizeResult.cpp
+++ C++/2259_RemoveDigitFromNumberToMaximizeResult.cpp
@@ -41,9 +41,9 @@ class Solution {
 public:
     string removeDigit(string number, char digit) {
         string res = "";
-        for(int i=0; i<number.size(); i++){
+        for(int i = 0; i < number.size(); i++){
             if(number[i] == digit){
-                string temp = number.substr(0, i) + number.substr(i+1);
+                string temp = number.substr(0, i) + number.substr(i + 1);
                 res = max(res, temp);
             }
         }
