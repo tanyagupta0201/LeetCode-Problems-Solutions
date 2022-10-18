2b864fd3e7148b5110e3ab540f3fadb0b24339df
Mahima Chauhan <mahima19197@gmail.com>
2022-10-18 17:20:15 +0530

    added formatted solution to issue #235

--- C++/463_IslandPerimeter.cpp
+++ C++/463_IslandPerimeter.cpp
@@ -1,12 +1,12 @@
-//Name:Mahima Chauhan
-//Date:14/10/2022
+// Name : Mahima Chauhan
+// Date : 14/10/2022
 class Solution {
   public:
     int islandPerimeter(vector < vector < int >> & grid) {
-      int m = grid.size(); //row of grid
-      int n = grid[0].size(); //column of grid
+      int m = grid.size(); // row of grid
+      int n = grid[0].size(); // column of grid
       int cnt = 0; // store perimeter 
-      //to calculate the perimeter we need to check the value of 4 sides of the land that is if it is water or not
+      // to calculate the perimeter we need to check the value of 4 sides of the land that is if it is water or not
       for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
           if (grid[i][j] == 1) {
