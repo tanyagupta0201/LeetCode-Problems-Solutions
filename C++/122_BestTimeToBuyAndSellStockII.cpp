f966d5452f27c3c2412badf7106c53a0e093b4ab
Harshit Jain <55655148+Harshit1487@users.noreply.github.com>
2022-10-20 09:16:48 +0530

    Update Added solution to the issue #335

--- C++/122_BestTimeToBuyAndSellStockII.cpp
+++ C++/122_BestTimeToBuyAndSellStockII.cpp
@@ -1,5 +1,5 @@
-//Name: Harshit Jain
-//Date: 19/10/2022
+// Name: Harshit Jain
+// Date: 19/10/2022
 
 /*
 You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
@@ -11,33 +11,20 @@ Find and return the maximum profit you can achieve.
 
 */
 
-#include<bits/stdc++.h>
-#include <iostream>
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
 
-using namespace std;
-
-int maxProfit(vector<int>& prices) {
-    //n: size of given array
-    //s:sum of all the profit 
-    int n=prices.size(),s=0;
-    
-    //run loop from 1 to n-1
-    for(int i=1;i<n;i++){
-        if(prices[i]>prices[i-1])
-            s+=prices[i]-prices[i-1];  
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+         // n: size of given vector
+         // s:sum of all the profit 
+        
+        int n=prices.size(),s=0;
+        
+        for(int i=1;i<n;i++){
+            if(prices[i]>prices[i-1])
+                s+=prices[i]-prices[i-1];  
+        }
+        return s;
     }
-    
-    return s;
-}
-
-int main()
-{
-    vector<int>prices = {7,1,5,3,6,4};
-    
-    //call function to get result
-    int res = maxProfit(prices);
-    
-    cout << res;
-
-    return 0;
-}
+};
