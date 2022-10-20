90bbf8ad4bc6fb3e2dca46fd21dfdfd2e01945df
chandantw <114911770+chandantw@users.noreply.github.com>
2022-10-20 14:04:29 +0530

    Refactored The Code

--- C++/104_MaximumDepthOfBinaryTree.cpp
+++ C++/104_MaximumDepthOfBinaryTree.cpp
@@ -1,12 +1,13 @@
-//Name: Chandan Sahu
-//Date: 18/10/2022
+// Name: Chandan Sahu
+// Date: 18/10/2022
 class Solution {
   public:
     /* Compute the "maxDepth" of a tree -- the number of
         nodes along the longest path from the root node
         down to the farthest leaf node.*/
     int maxDepth(TreeNode * root) {
-      if (root == NULL) return 0;
+      if (root == NULL) 
+         return 0;
       /* compute the depth of each subtree */
       int left = maxDepth(root -> left);
       int right = maxDepth(root -> right);
