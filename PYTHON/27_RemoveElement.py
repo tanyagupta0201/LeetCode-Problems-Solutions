6f4c40624a271439b39fe0fbb07eefa13a22f70c
Akruti <aviternal20@gmail.com>
2022-10-20 21:12:58 +0530

    Changes made by akruti, added a new python problem

---
+++ PYTHON/27_RemoveElement.py
@@ -0,0 +1,22 @@
+# Name - Akruti Sarangi
+# Date - 20/10/2022
+
+class Solution(object):
+    def removeElement(self, nums, val):
+        ls = len(nums)
+        if ls == 0:
+            return ls
+        count = 0
+        index = 0
+        while index < ls - count:
+            if nums[index] == val:
+                nums[index] = nums[ls - 1 - count]
+                count += 1
+            else:
+                index += 1
+        return ls - count
+
+if __name__ == '__main__':
+    # begin
+    s = Solution()
+    print s.removeElement([1], 1)
