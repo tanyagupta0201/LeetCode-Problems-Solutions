2605a603af8fd9124ddd9c8a3229366f6ac81439
roy2pushkar <roy2000pushkar.com>
2022-10-21 10:31:52 +0530

    633_SumofSquareNumbers.cpp

--- C++/633_SumofSquareNumbers.cpp
+++ C++/633_SumofSquareNumbers.cpp
@@ -1,5 +1,17 @@
 //Name :- Pushkar Roy
 //Date :- 05-10-2022
+/*
+Given a non-negative integer c, decide whether there're two integers a and b such that a2 + b2 = c.
+
+Example 1:
+Input: c = 5
+Output: true
+Explanation: 1 * 1 + 2 * 2 = 5
+
+Example 2:
+Input: c = 3
+Output: false
+ */
 
 class Solution {
   public:
@@ -8,36 +20,26 @@ class Solution {
       long long int l = 0;
 
       long long int r = sqrt(c);
-
-    // check condition for loop
-
+      // check condition for loop
       while (l <= r) {
-
-    // find range for checking
-
-        long long int value = (l * l) + (r * r);
-
-    // if value matchecd with given number return true
-
-        if (value == c) {
+      // find range for checking
+      long long int value = (l * l) + (r * r);
+      // if value matchecd with given number return true
+      if (value == c) {
           return true;
         }
-
-    // if value greater than given number
-
-        else if (value > c) {
+      // if value greater than given number
+      else if (value > c) {
           r--;
         }
-
-    // if value less than given number
-
+      // if value less than given number
         else if (value < c) {
           l++;
         }
       }
-
-   // if value not matched
-
+     // if value not matched
       return false;
     }
 };
+//TC :- O(logn) because we use Binary search technique
+//SC :- O(1)  store nothing
