c6bf7770a52eab3f156e91c488e4659c5a270fb2
Siddhartha Mishra <72318545+siddharthamishra1207@users.noreply.github.com>
2022-10-21 17:19:06 +0530

    Add files via upload

---
+++ JAVA/202_HappyNumber.java
@@ -0,0 +1,60 @@
+//name:-
+//date:- 
+/*
+ * Leetcode:- 202 ( Happy Number)
+ * 
+ * Write an algorithm to determine if a number n is happy.
+
+A happy number is a number defined by the following process:
+
+Starting with any positive integer, replace the number by the sum of the squares of its digits.
+Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+Those numbers for which this process ends in 1 are happy.
+Return true if n is a happy number, and false if not.
+
+ 
+
+Example 1:
+
+Input: n = 19
+Output: true
+Explanation:
+12 + 92 = 82
+82 + 22 = 68
+62 + 82 = 100
+12 + 02 + 02 = 1
+Example 2:
+
+Input: n = 2
+Output: false
+ 
+
+Constraints:
+
+1 <= n <= 231 - 1 
+ * 
+ */
+
+ //solution 
+
+ class Solution {
+    public boolean isHappy(int n) {
+        int slow = n, fast = n;
+        do {
+            slow = cal(slow);
+            fast = cal(cal(fast));
+        } while (slow != fast);
+        return slow == 1;
+        
+    }
+    public int cal(int n) {
+        int res = 0;
+        while (n > 0) {
+            int d = n % 10;
+            res += d * d;
+            n /= 10;
+        }
+        return res;
+    }
+    
+}
