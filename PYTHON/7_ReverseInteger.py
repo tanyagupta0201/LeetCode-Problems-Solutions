ac3031c1bbfa34c6b51cc97e1564b7bc6c9de030
Shreyansh Goyal <110629672+Shreyanshgl25@users.noreply.github.com>
2022-10-22 11:35:50 +0530

    code for reverse a number

--- PYTHON/7_ReverseInteger.py
+++ PYTHON/7_ReverseInteger.py
@@ -2,17 +2,5 @@
 # Date: 21/10/22
 
 number = int(input("Enter the integer number: "))  
-  
-# Initiate value to null  
-revs_number = 0  
-  
-# reverse the integer number using the while loop  
-  
-while (number > 0):  
-    # Logic  
-    remainder = number % 10  
-    revs_number = (revs_number * 10) + remainder  
-    number = number // 10  
-  
-# Display the result  
-print("The reverse number is : {}".format(revs_number))  
+  #The result will Print the reverse number that the user will enter.
+print(str (number)[::-1]) 
