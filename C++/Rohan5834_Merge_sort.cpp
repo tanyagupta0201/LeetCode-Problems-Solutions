7b20ea926f36ddf99b697068739c88e8583b86ad
Rohan5834 <116454690+Rohan5834@users.noreply.github.com>
2022-10-22 23:03:01 +0530

    Create Rohan5834_Merge_sort.cpp

---
+++ C++/Rohan5834_Merge_sort.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+void merge(vector& nums1, int m, vector& nums2, int n) {
+int i = m - 1, j = n - 1, tar = m + n - 1;
+while (j >= 0) {
+nums1[tar--] = i >= 0 && nums1[i] > nums2[j] ? nums1[i--] : nums2[j--];
+}
+}
+};
