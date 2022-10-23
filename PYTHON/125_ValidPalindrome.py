a01d140b9321ff3b17c8e694a6447d4bac130e33
Esan Raj <ishanraj845@gmail.com>
2022-10-23 15:08:18 +0530

    updated_2

--- PYTHON/125_ValidPalindrome.py
+++ PYTHON/125_ValidPalindrome.py
@@ -1,4 +1,4 @@
-#Name - Esan  Raj
+# Name - Esan  Raj
 # Date -22/10/2022
 
 
@@ -6,8 +6,14 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         a = ""
         for x in [*s]:
-            if x.isalpha(): #comparing x in given string that ,it is a character or not 
-                a+= x.lower() #converting the text to lower case and then add the character to string 'a'
-            if x.isnumeric(): #comparing x in given string that, it is a number or not
-                a+= x #adding the character in the string 'a'
-        return a ==a[::-1] #comparing the string with itself by traversing it in reverse order for palindrome.
+            if (
+                x.isalpha()
+            ):  # comparing x in given string that ,it is a character or not
+                a += (
+                    x.lower()
+                )  # converting the text to lower case and then add the character to string 'a'
+            if x.isnumeric():  # comparing x in given string that, it is a number or not
+                a += x  # adding the character in the string 'a'
+        return (
+            a == a[::-1]
+        )  # comparing the string with itself by traversing it in reverse order for palindrome.
