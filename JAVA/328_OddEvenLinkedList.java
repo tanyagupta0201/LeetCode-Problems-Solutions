2e7a356a65de18817ac3a65ba45a2cc36af0b588
Ayush Sharma <58669560+ayush26sharma@users.noreply.github.com>
2022-10-24 16:19:02 +0530

    Update 328_OddEvenLinkedList.java

--- JAVA/328_OddEvenLinkedList.java
+++ JAVA/328_OddEvenLinkedList.java
@@ -29,44 +29,47 @@ Space Complexity - O(1)
 */
 
 
-
 class ListNode {
     int val;
     ListNode next;
     ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    ListNode(int val) {
+        this.val = val;
+    }
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
 }
 
-
-class Solution{
+class Solution {
     public ListNode oddEvenList(ListNode head) {
         ListNode even = new ListNode(-1);
         ListNode odd = new ListNode(-1);
         ListNode even1 = even;
         ListNode odd2 = odd;
         ListNode curr = head;
-        if(head==null || head.next==null) return head;
+        if (head == null || head.next == null) return head;
         ListNode curr1 = head.next;
-        while(curr!=null && curr.next!=null){
-            odd.next=new ListNode(curr.val);
-            curr=curr.next.next;
-            odd=odd.next;
+        while (curr != null && curr.next != null) {
+            odd.next = new ListNode(curr.val);
+            curr = curr.next.next;
+            odd = odd.next;
         }
-        if(curr!=null){
-            odd.next=new ListNode(curr.val);
-            odd=odd.next;
+        if (curr != null) {
+            odd.next = new ListNode(curr.val);
+            odd = odd.next;
         }
-        while(curr1!=null && curr1.next!=null){
-            even.next=new ListNode(curr1.val);
-            curr1=curr1.next.next;
-            even=even.next;
+        while (curr1 != null && curr1.next != null) {
+            even.next = new ListNode(curr1.val);
+            curr1 = curr1.next.next;
+            even = even.next;
         }
-        if(curr1!=null){
-            even.next=new ListNode(curr1.val);
-            even=even.next;
+        if (curr1 != null) {
+            even.next = new ListNode(curr1.val);
+            even = even.next;
         }
-        odd.next=even1.next;
+        odd.next = even1.next;
         return odd2.next;
     }
 }
