43736d9c98db39e1b4a92bc006e00278b3b6ba5e
Nandini-Sri <116311748+Nandini-Sri@users.noreply.github.com>
2022-10-25 14:10:04 +0530

    Update 932_BeautifulArray.java

--- JAVA/932_BeautifulArray.java
+++ JAVA/932_BeautifulArray.java
@@ -31,28 +31,28 @@ Constraints:
 
  // Solution 
 
- class Solution {
-    Map < Integer, int[] > memo;
-    public int[] beautifulArray(int N) {
-      memo = new HashMap();
-      return f(N);
-    }
-  
-    public int[] f(int N) {
-      if (memo.containsKey(N))
-        return memo.get(N);
-  
-      int[] ans = new int[N];
-      if (N == 1) {
-        ans[0] = 1;
-      } else {
-        int t = 0;
-        for (int x: f((N + 1) / 2)) // For Odds
-          ans[t++] = 2 * x - 1;
-        for (int x: f(N / 2)) // For Evens
-          ans[t++] = 2 * x;
-      }
-      memo.put(N, ans);
-      return ans;
-    }
-  }
+  class Solution {
+   Map < Integer, int[] > memo;
+   public int[] beautifulArray(int N) {
+     memo = new HashMap();
+     return f(N);
+   }
+
+   public int[] f(int N) {
+     if (memo.containsKey(N))
+       return memo.get(N);
+
+     int[] ans = new int[N];
+     if (N == 1) {
+       ans[0] = 1;
+     } else {
+       int t = 0;
+       for (int x: f((N + 1) / 2)) // For Odds
+         ans[t++] = 2 * x - 1;
+       for (int x: f(N / 2)) // For Evens
+         ans[t++] = 2 * x;
+     }
+     memo.put(N, ans);
+     return ans;
+   }
+ }
