ffc44feeea6d8e8804842e8514e469a3b812069a
Tanya Gupta <66638392+tanyagupta0201@users.noreply.github.com>
2022-10-25 13:08:09 +0530

    Update 383_RansomNote.cpp

--- C++/383_RansomNote.cpp
+++ C++/383_RansomNote.cpp
@@ -3,25 +3,32 @@
 
 class Solution {
 public:
-    bool canConstruct(string ransomNote, string magazine) {
+    bool canConstruct(string ransomNote, string magazine) 
+    {
         int n = ransomNote.length();
         int m = magazine.length();
         
-        if(n > m){
+        if(n > m)
+        {
             return false;       // If length of ransomNote is greater than length of magazine return false.
         }
         
-        map<char,int> m1;        // Create a map to store the frequencies of each character of string magazine.
+        map<char, int> m1;        // Create a map to store the frequencies of each character of string magazine.
         
-        for(int i = 0; i < m; i++){
+        for(int i = 0; i < m; i++)
+        {
             m1[magazine[i]]++;      // Store the frequencies of string.
         }
     
-        for(int i = 0; i < n; i++){
-            if(m1[ransomNote[i]] > 0){        // If character of ransomeNote is present in map, then deduct the frequency of character.
+        for(int i = 0; i < n; i++)
+        {
+            if(m1[ransomNote[i]] > 0)
+            {        
+                // If character of ransomeNote is present in map, then deduct the frequency of character.
                 m1[ransomNote[i]]--;
             }
-            else{
+            else
+            {
                 return false;
             }
         }
