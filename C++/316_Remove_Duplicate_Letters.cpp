5ebcc77283f13d3f000c24d5553032a56f287904
RajSpeed <116724067+RajSpeed@users.noreply.github.com>
2022-10-26 22:17:55 +0530

    Update 316_Remove_Duplicate_Letters.cpp

--- C++/316_Remove_Duplicate_Letters.cpp
+++ C++/316_Remove_Duplicate_Letters.cpp
@@ -2,7 +2,8 @@
 // Date: 26/10/2022
 
 /*
-Given a string s, return the lexicographically smallest subsequence of s that contains all the distinct characters of s exactly once.
+Given a string s, remove duplicate letters so that every letter appears once and only once.
+You must make sure your result is the smallest in lexicographical order among all possible results
 */
 
 // This question is the same as 1081: https://leetcode.com/problems/smallest-subsequence-of-distinct-characters/
