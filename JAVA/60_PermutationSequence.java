ad8fe729adfef17fde86ea9e5e6d255d5b708d72
Abhay Jagtap <91551930+Abhay-Jagtap4@users.noreply.github.com>
2022-10-29 13:15:19 +0530

    Updated 60. Permutation Sequence

--- JAVA/60_PermutationSequence.java
+++ JAVA/60_PermutationSequence.java
@@ -1,3 +1,6 @@
+// Name : Abhay Jagtap
+// Date : 29/10/2022
+
 /*
 Problem :
 The set [1, 2, 3, ..., n] contains a total of n! unique permutations. By listing and labeling all of the permutations in order, we get the following sequence for n = 3:
@@ -9,9 +12,13 @@ The set [1, 2, 3, ..., n] contains a total of n! unique permutations. By listing
 "321"
 Given n and k, return the kth permutation sequence.
 
-Ex :
+Example 1 :
 Input: n = 3, k = 3
 Output: "213"
+
+Example 2 :
+Input: n = 4, k = 9
+Output: "2314"
 
 Time Complexity : O(n)
 Space Complexity : O(n)
